a15988114ff0b1117e8d77f9008048a06cf5877b
Nikita Myroniuk <nikitamix1sstt@gmail.com>
2026-04-24 23:47:10 +0200

    validation methods adding

---
+++ src/main/java/Util/LetterValidatorImpl.java
@@ -0,0 +1,9 @@
+package Util;
+
+public class LetterValidatorImpl implements LetterValidator{
+
+    @Override
+    public boolean isValid() {
+        return false;
+    }
+}
--- src/main/java/Util/NameValidatorImpl.java
+++ src/main/java/Util/NameValidatorImpl.java
@@ -1,12 +1,13 @@
 package Util;
 
 public class NameValidatorImpl implements NameValidator {
+    private static final String NAME_REGEX = "^[A-Za-zА-Яа-я\\s\\-]+$";
+
     @Override
     public boolean isValid(String name) {
         if (name == null || name.isEmpty()) {
             return false;
         }
-
-
+        return name.matches(NAME_REGEX);
     }
 }
